6c22822c59565f582bfadb699d7824436a14e356
yungd1plomat <aynur.musin.06@mail.ru>
2024-01-24 21:57:56 +0500

    change install script

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 sudo NEEDRESTART_MODE=a apt update
 sudo NEEDRESTART_MODE=a apt install python3-pip -y
 pip install PySocks
-cd data
-sudo nano accounts.txt
+crontab -r
 crontab -l > mycron
 echo "0 3 * * * /usr/bin/python3 /home/ubuntu/RtanksTools/nuker.py '/home/ubuntu/RtanksTools/data/errors.txt' >> /home/ubuntu/RtanksTools/cron.log 2>&1" >> mycron
 crontab mycron
 rm mycron
+sudo nano data/accounts.txt

a2fd527bcdcbbcbedbadb90af99ecaacf4fcb361
yungd1plomat <aynur.musin.06@mail.ru>
2024-01-24 22:58:50 +0500

    change cron command

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo NEEDRESTART_MODE=a apt install python3-pip -y
 pip install -r requirements.txt
 crontab -r
 crontab -l > mycron
-echo "0 3 * * * /usr/bin/python3 /home/ubuntu/RtanksTools/nuker.py '/home/ubuntu/RtanksTools/data/errors.txt' >> /home/ubuntu/RtanksTools/cron.log 2>&1" >> mycron
+echo "0 3 * * * /usr/bin/python3 /home/ubuntu/RtanksTools/nuker.py '/home/ubuntu/RtanksTools/data' >> /home/ubuntu/RtanksTools/cron.log 2>&1" >> mycron
 crontab mycron
 rm mycron
 sudo nano data/accounts.txt

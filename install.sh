2ba8b0a9abf15f9d8cd05eeb32f2fcaaefe115e1
yungd1plomat <aynur.musin.06@mail.ru>
2024-01-24 22:44:10 +0500

    add requirements install to script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo NEEDRESTART_MODE=a apt update
 sudo NEEDRESTART_MODE=a apt install python3-pip -y
-pip install PySocks
+pip install -r requirements.txt
 crontab -r
 crontab -l > mycron
 echo "0 3 * * * /usr/bin/python3 /home/ubuntu/RtanksTools/nuker.py '/home/ubuntu/RtanksTools/data/errors.txt' >> /home/ubuntu/RtanksTools/cron.log 2>&1" >> mycron
